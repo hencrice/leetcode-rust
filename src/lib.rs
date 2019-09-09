a198d8cace5acff30b6cc3af112d8f8a72978ad3
Yen-Lin Chen <3822365+hencrice@users.noreply.github.com>
2019-09-08 22:27:09 -0700

    Solved the reverse integer problem

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod q00001_two_sum;
+pub mod q00002_reverse_integer;
---
+++ src/q00002_reverse_integer.rs
@@ -0,0 +1,44 @@
+pub fn reverse(x: i32) -> i32 {
+    let is_negative = x < 0;
+    let mut v = x.abs();
+
+    let mut rev_x: i32 = 0;
+
+    while v > 0 {
+        let digit = v % 10;
+
+        match rev_x.checked_mul(10) {
+            Some(res) => rev_x = res,
+            None => return 0,
+        }
+
+        match rev_x.checked_add(digit) {
+            Some(res) => rev_x = res,
+            None => return 0
+        }
+
+        v = v / 10;
+    }
+
+    if is_negative {-rev_x} else {rev_x}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reverse_positive() {
+        assert_eq!(reverse(123), 321);
+    }
+
+    #[test]
+    fn reverse_negative() {
+        assert_eq!(reverse(-123), -321);
+    }
+
+    #[test]
+    fn reverse_overflows() {
+        assert_eq!(reverse(1534236469), 0);
+    }
+}
