615a339492553d53c2cdff8309ba49a444707709
Yen-Lin Chen <3822365+hencrice@users.noreply.github.com>
2019-09-08 20:28:06 -0700

    Added solution for the two sum problem

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+pub mod q00001_two_sum;
---
+++ src/q00001_two_sum.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let value_to_index = nums.iter().zip(0..nums.len() as i32).collect::<HashMap<_, _>>();
+    for (i, &v) in nums.iter().enumerate() {
+        match value_to_index.get(&(target - v)) {
+            Some(&index) if (i as i32) != index => return vec![(i as i32), index],
+            _ => continue
+        };
+    }
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(two_sum(vec![2, 2, 3], 4), vec![0, 1]);
+    }
+}
